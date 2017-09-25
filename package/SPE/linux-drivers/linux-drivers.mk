1f3098e78f5013a274d8338c9a6ad1ee4ad4a06d
JinhuaWu <olineboy@163.com>
2017-09-25 23:13:50 +0800

    Support new pacakge for linux-drivers exmaples

---
+++ package/SPE/linux-drivers/linux-drivers.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# LINUX_DRIVERS
+#
+################################################################################
+
+LINUX_DRIVERS_VERSION = 2c0fb33ed56ca1379ff03ffb8723b3f18f362eee
+LINUX_DRIVERS_SITE = https://github.com/JinhuaW/linux_drivers.git
+LINUX_DRIVERS_SITE_METHOD = git
+LINUX_DRIVERS_INSTALL_MOD_DIR = spe
+LINUX_DRIVERS_DEPENDENCIES = linux
+LINUX_DRIVERS_LICENSE = CPLv2 
+LINUX_DRIVERS_LICENSE_FILES = COPYING 
+LINUX_DRIVERS_INSTALL_STAGING = YES
+
+LINUX_DRIVERS_MAKE_OPTS +=
+
+define LINUX_DRIVERS_BUILD_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules \
+		$(LINUX_DRIVERS_MAKE_OPTS) \
+		KBUILD_EXTRA_SYMBOLS=$(LINUX_DRIVERS_EXTRA_SYMBOLS)
+endef
+
+define LINUX_DRIVERS_INSTALL_TARGET_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install \
+		INSTALL_MOD_STRIP=1 \
+		INSTALL_MOD_DIR=$(LINUX_DRIVERS_INSTALL_MOD_DIR) \
+		$(LINUX_DRIVERS_MAKE_OPTS)
+endef
+
+define LINUX_DRIVERS_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) headers_install \
+		DESTDIR=$(STAGING_DIR) \
+		$(LINUX_DRIVERS_MAKE_OPTS)
+endef
+
+$(eval $(generic-package))
