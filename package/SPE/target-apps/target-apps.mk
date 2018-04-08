511015480f5accf32a8c42779322629822fab4a1
JinhuaWu <olineboy@163.com>
2018-04-08 15:04:26 +0800

    fix target_apps compile libuio dependency issue

--- package/SPE/target-apps/target-apps.mk
+++ package/SPE/target-apps/target-apps.mk
@@ -8,7 +8,7 @@ TARGET_APPS_VERSION = dbd49a27bac5b82f0c6d9e3646902b799d63dcfb
 TARGET_APPS_SITE = https://github.com/JinhuaW/target-apps.git 
 TARGET_APPS_SITE_METHOD = git
 
-TARGET_APPS_DEPENDENCIES = libfuse
+TARGET_APPS_DEPENDENCIES = libfuse libuio
 
 TARGET_APPS_MAKE_OPTS += DESTDIR?=$(TARGET_DIR)/usr/bin
 
